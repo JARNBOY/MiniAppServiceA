e10927c755b5ea062a297c12136529e2ce1dc348
Papon <papon.s@lawyerplusgroup.com>
2023-01-26 11:12:41 +0700

    Initial Commit

---
+++ MiniAppServiceA/MiniAppServiceA.h
@@ -0,0 +1,18 @@
+//
+//  MiniAppServiceA.h
+//  MiniAppServiceA
+//
+//  Created by Papon Supamongkonchai on 26/1/2566 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MiniAppServiceA.
+FOUNDATION_EXPORT double MiniAppServiceAVersionNumber;
+
+//! Project version string for MiniAppServiceA.
+FOUNDATION_EXPORT const unsigned char MiniAppServiceAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MiniAppServiceA/PublicHeader.h>
+
+
